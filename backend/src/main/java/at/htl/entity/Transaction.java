52105dafd0b54def99f747cf36c1a4b8f4dcb8a4
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-09-24 10:48:11 +0200

    implemented Transaction Entity

--- backend/src/main/java/at/htl/entity/Transaction.java
+++ backend/src/main/java/at/htl/entity/Transaction.java
@@ -1,4 +1,66 @@
 package at.htl.entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Plant plant;
+    @ManyToOne
+    private Customer customer;
+    private LocalDate dateOfTransaction;
+
+    //region Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Plant getPlant() {
+        return plant;
+    }
+
+    public void setPlant(Plant plant) {
+        this.plant = plant;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDate getDateOfTransaction() {
+        return dateOfTransaction;
+    }
+
+    public void setDateOfTransaction(LocalDate dateOfTransaction) {
+        this.dateOfTransaction = dateOfTransaction;
+    }
+    //endregion
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Transaction)) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id) && Objects.equals(plant, that.plant) && Objects.equals(customer, that.customer) && Objects.equals(dateOfTransaction, that.dateOfTransaction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, plant, customer, dateOfTransaction);
+    }
 }
