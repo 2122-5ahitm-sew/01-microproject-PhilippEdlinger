8884ca67f1bf3574654c69ba8089c2dbd5432823
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-09-24 10:47:37 +0200

    implemnted Customer Entity

--- backend/src/main/java/at/htl/entity/Customer.java
+++ backend/src/main/java/at/htl/entity/Customer.java
@@ -1,4 +1,76 @@
 package at.htl.entity;
 
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Transaction> transactionList;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String firstName, String lastName, List<Transaction> transactionList) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.transactionList = transactionList;
+    }
+
+    //region Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
+    //endregion
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName) && Objects.equals(transactionList, customer.transactionList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, transactionList);
+    }
 }
