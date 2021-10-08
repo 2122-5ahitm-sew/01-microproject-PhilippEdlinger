a7469660313ec6213595881ad2b413ece81713e4
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-08 09:37:03 +0200

    implemented CropAreaRepository

---
+++ backend/src/main/java/at/htl/repository/CropAreaRepository.java
@@ -0,0 +1,10 @@
+package at.htl.repository;
+
+import at.htl.entity.CropArea;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CropAreaRepository implements PanacheRepositoryBase {
+}
