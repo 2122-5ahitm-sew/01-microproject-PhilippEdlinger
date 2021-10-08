31105e5258a6fb7317f1550dadfa55f69c35dedf
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-08 09:36:02 +0200

    implemented CareActionRepository

--- backend/src/main/java/at/htl/repository/CareActionRepository.java
+++ backend/src/main/java/at/htl/repository/CareActionRepository.java
@@ -1,11 +1,9 @@
 package at.htl.repository;
 
-import at.htl.entity.Customer;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.transaction.Transactional;
 
 @ApplicationScoped
-public class CustomerRepository implements PanacheRepositoryBase {
+public class CareActionRepository implements PanacheRepositoryBase {
 }
