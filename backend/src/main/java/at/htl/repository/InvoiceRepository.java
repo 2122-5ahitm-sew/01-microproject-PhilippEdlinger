ef001d2b2a7fbb4553adf3b6d6828ead877f6953
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-08 09:37:57 +0200

    implemented InvoiceRepository

---
+++ backend/src/main/java/at/htl/repository/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package at.htl.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class InvoiceRepository implements PanacheRepositoryBase {
+}
