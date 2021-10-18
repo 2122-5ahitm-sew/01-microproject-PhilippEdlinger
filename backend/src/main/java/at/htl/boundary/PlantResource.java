4877778d4abbdbe79f79687c89e908ce35e6a151
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-18 14:07:27 +0200

    added createPlant endpoint

--- backend/src/main/java/at/htl/boundary/PlantResource.java
+++ backend/src/main/java/at/htl/boundary/PlantResource.java
@@ -1,7 +1,38 @@
 package at.htl.boundary;
 
-import javax.ws.rs.Path;
+import at.htl.entity.Plant;
+import at.htl.entity.PlantType;
+import at.htl.repository.PlantRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/plant")
 public class PlantResource {
+
+    @Inject
+    PlantRepository plantRepository;
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createPlant(
+            @FormParam("plantName") String plantName,
+            @FormParam("plantType") String plantType
+    ){
+
+        Plant plant = new Plant();
+        plant.setPlantName(plantName);
+        plant.setPlantType(PlantType.valueOf(plantType));
+
+        plantRepository.save(plant);
+
+        return Response
+                .accepted(plant)
+                .build();
+
+    }
+
 }
--- backend/src/main/java/at/htl/repository/PlantRepository.java
+++ backend/src/main/java/at/htl/repository/PlantRepository.java
@@ -4,7 +4,14 @@ import at.htl.entity.Plant;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class PlantRepository implements PanacheRepositoryBase<Plant,Long> {
+
+    @Transactional
+    public void save(Plant plant){
+        getEntityManager().persist(plant);
+    }
+
 }
